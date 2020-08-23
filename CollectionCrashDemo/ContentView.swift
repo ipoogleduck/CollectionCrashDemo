58a58182d5f7facfbe54f2094b1f659d861c33c0
ipoogleduck <ipoogleduck@gmail.com>
2020-08-22 23:56:14 -0700

    Initial Commit

---
+++ CollectionCrashDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CollectionCrashDemo
+//
+//  Created by Oliver Elliott on 8/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
